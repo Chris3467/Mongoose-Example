2aa41283267b5208adf2f1fa0464ab75611f3f1c
Chris3467 <chris.collado815@gmail.com>
2024-12-03 15:04:36 -0500

    server is running and connected

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import db from "./db/conn.mjs";
+const PORT = process.env.PORT || 5052;
+
+const app = express();
+
+app.use(express.json());
+
+// start the express server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
